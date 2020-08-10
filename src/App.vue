3269f35ab129946440752c9d2fc6f0e6a2ecf673
Alan <alanalvarez98.aa@gmail.com>
2020-08-10 14:05:27 -0500

    Creacion del Repositorio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Navbar></Navbar>
+      <v-container>
+
+        <v-row>
+          <v-card
+          class="mx-auto my-3"
+          max-width="350"
+          v-for="noticia in news"
+          :key="noticia.url"
+          >
+            <v-img
+              height="200px"
+              :src="noticia.urlToImage"
+            >
+
+            </v-img>
+
+            <v-card-title>
+              {{noticia.title}}
+            </v-card-title>
+
+            <v-card-subtitle>
+              {{noticia.author}}
+            </v-card-subtitle>
+
+            <v-card-text>
+              {{noticia.description}}
+            </v-card-text>
+
+            <v-card-subtitle>
+              {{noticia.publishedAt.slice(0,10)}}
+            </v-card-subtitle>
+
+            <v-card-action>
+              <v-btn
+                color="orange"
+                text
+                target="_blank"
+                :href="noticia.url"
+              >
+                Ver Noticia
+              </v-btn>
+            </v-card-action>
+            
+          </v-card>
+        </v-row>
+      </v-container>
+      
+
+
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Navbar from './components/Navbar'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    Navbar
+  },
+
+  data: () => ({
+    news: {}
+  }),
+
+  methods: {
+
+    async getNews(){
+      let apiURL = 'http://newsapi.org/v2/top-headlines?country=mx&category=technology&apiKey=19aa6dd2908a492cbb301ed1f2936a02'
+
+      let noticias = await this.axios.get(apiURL)
+
+      this.news = noticias.data.articles
+      console.log(this.news)
+
+    }
+
+  },
+
+  created(){
+    this.getNews()
+  }
+
+
+
+
+};
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+     <v-app-bar
+          dark
+          color="primary"
+     >
+          <v-img
+               alt="Bee Logo"
+               class="shrink mr-2"
+               contain
+               src="../assets/bee-logo.png"
+               width="40"
+          >
+
+          </v-img>
+          <h1>BEE NEWS</h1>
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+               href="https://beedevelopers.dev"
+               target="_blank"
+               text
+          >
+               <v-icon class="mr-2">fas fa-link</v-icon>
+               <span> Web de BeeDevelopers</span>
+          </v-btn>
+     </v-app-bar>
+</template>
+
+<script>
+export default {
+     name: 'Navbar'
+}
+</script>
